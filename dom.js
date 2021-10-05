6a4d71a634c258c53621ddc3a66378d55556d020
Om Prakash Thakur <om7271prakash@gmail.com>
2021-10-05 07:13:56 +0530

    dom and bom added

---
+++ dom.js
@@ -0,0 +1,35 @@
+// Window
+// =========================================================
+// global object
+
+// -------------------------------------------------------------
+
+// Document
+// =========================================================
+// child of window
+
+
+// for head 
+// document.head
+
+// for body
+// document.body.childNodes
+// document.body.children
+// document.body.hasChildNodes
+
+// document.body.firstChild     // also includes text as child
+// document.body.firstElementChild   // include first element
+
+// let body = document.body;
+// let id = body.getElementById('id_name');
+// let cls = body.getElementsByClassNames('class_name');
+// let tg = body.getElementsByTagName('tag_name');
+// let nameElm = body.getElementsByTagName('form_name');
+
+// querySelector()
+// querySelectorAll()
+// -------------------------------------------
+
+// .  => className
+// # => idName
+
